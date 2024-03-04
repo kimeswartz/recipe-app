b335e4fd4811f2d4a1cb3897336943e8ded43f8c
Kim Swartz <kimswartz@MacBook-Pro-som-tillhor-Kim-2.local>
2024-03-04 19:50:08 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import DataDisplay from "./components/DisplayRecipes";
+import UploadRecipeComponent from "./components/UploadRecipe";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+
+          <UploadRecipeComponent />
+          <DataDisplay />
+        </div>
+
+    </>
+  )
+}
+
+export default App
