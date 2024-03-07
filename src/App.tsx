198316535561709ebafb76c1fb9466af47adb6c0
CasperWLX <hampuskallberg@gmail.com>
2024-03-07 19:01:17 +0100

    please work

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,19 @@
 import "./App.css";
 import DataDisplay from "./components/DisplayRecipes";
 import UploadRecipeComponent from "./components/UploadRecipe";
-<<<<<<< HEAD
-=======
 import CategorySearch from "./components/CategorySearch";
 import RecipeSearch from "./components/FindRecipe";
->>>>>>> d32348786b9b46f70a540931719b7d303ebdf41a
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="card">
-<<<<<<< HEAD
 
-=======
         <RecipeSearch />
->>>>>>> d32348786b9b46f70a540931719b7d303ebdf41a
-          <UploadRecipeComponent />
-          <DataDisplay />
-        </div>
+        <UploadRecipeComponent />
+        <DataDisplay />
+      </div>
 
     </>
   );
