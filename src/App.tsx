f5bfa3133dd8805d5d021d5ce42f292566feb917
aliceh0155 <hofverbergalice@gmail.com>
2024-03-11 12:41:25 +0100

    merge solved

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,14 @@ import DataDisplay from "./components/DisplayRecipes";
 import UploadRecipeComponent from "./components/UploadRecipe";
 import RecipeSearch from "./components/SearchRecipe";
 import CategorySearch from "./components/CategorySearch";
-<<<<<<< HEAD
 import DeleteRecipe from "./components/DeleteRecipe";
-=======
->>>>>>> 0166bf65889e8806b8a5153c3cf6ed75832e484c
 
 function App() {
   return (
     <>
       <div className="card">
-<<<<<<< HEAD
-=======
-        <RecipeSearch/>
->>>>>>> 0166bf65889e8806b8a5153c3cf6ed75832e484c
-        <UploadRecipeComponent />
         <RecipeSearch />
+        <UploadRecipeComponent />
         <DataDisplay />
         <CategorySearch />
         <DeleteRecipe />
