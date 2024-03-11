306f2822819aba2dfaed3d0e6a23982b4148d9bb
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2024-03-11 12:51:07 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,17 @@ import DataDisplay from "./components/DisplayRecipes";
 import UploadRecipeComponent from "./components/UploadRecipe";
 import RecipeSearch from "./components/SearchRecipe";
 import CategorySearch from "./components/CategorySearch";
-import DeleteRecipe from "./components/DeleteRecipe";
 
 function App() {
+
   return (
     <>
       <div className="card">
-        <RecipeSearch />
+        <RecipeSearch/>
         <UploadRecipeComponent />
         <DataDisplay />
         <CategorySearch />
-        <DeleteRecipe />
       </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> 0166bf65889e8806b8a5153c3cf6ed75832e484c
     </>
   );
 }
