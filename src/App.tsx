0166bf65889e8806b8a5153c3cf6ed75832e484c
CasperWLX <hampuskallberg@gmail.com>
2024-03-11 12:35:41 +0100

    removed merge arrows

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
 import "./App.css";
 import DataDisplay from "./components/DisplayRecipes";
 import UploadRecipeComponent from "./components/UploadRecipe";
-<<<<<<< HEAD
 import RecipeSearch from "./components/SearchRecipe";
-=======
 import CategorySearch from "./components/CategorySearch";
->>>>>>> 1726100fbadd742e2af492eec761eeaab1585839
 
 function App() {
 
   return (
     <>
       <div className="card">
-<<<<<<< HEAD
-          <RecipeSearch/>
-          <UploadRecipeComponent />
-          <DataDisplay />
-        </div>
-=======
+        <RecipeSearch/>
         <UploadRecipeComponent />
         <DataDisplay />
         <CategorySearch />
       </div>
->>>>>>> 1726100fbadd742e2af492eec761eeaab1585839
-
     </>
   );
 }
