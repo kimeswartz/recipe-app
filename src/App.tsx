5690f67c3cd57966df650763af763ee77ff45167
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2024-03-11 11:52:58 +0100

    sök funktion första exempel

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import DataDisplay from "./components/DisplayRecipes";
 import UploadRecipeComponent from "./components/UploadRecipe";
+import RecipeSearch from "./components/SearchRecipe";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div className="card">
-
+          <RecipeSearch/>
           <UploadRecipeComponent />
           <DataDisplay />
         </div>
--- src/interfaces/RecipeInterface.ts
+++ src/interfaces/RecipeInterface.ts
@@ -1,15 +1,15 @@
 export interface RecipeInterface {
-    title: string;
-    description: string;
-    ratings: number[]; // Assuming ratings is an array of numbers
-    imageUrl: string;
-    timeInMins: number;
-    categories: string[];
-    instructions: string[];
- 
-    ingredients: {
-      name: string;
-      amount: number;
-      unit: string;
-    }[];
+  title: string;
+  description: string;
+  ratings: number[]; // Assuming ratings is an array of numbers
+  imageUrl: string;
+  timeInMins: number;
+  categories: string[];
+  instructions: string[];
+
+  ingredients: {
+    name: string;
+    amount: number;
+    unit: string;
+  }[];
 }
