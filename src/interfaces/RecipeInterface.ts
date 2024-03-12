fc9d0e344cabdcfdf313d14aef7eb07d7bf66aa9
CasperWLX <hampuskallberg@gmail.com>
2024-03-12 16:08:58 +0100

    merge solved

--- src/interfaces/RecipeInterface.ts
+++ src/interfaces/RecipeInterface.ts
@@ -1,19 +1,4 @@
 export interface RecipeInterface {
-<<<<<<< HEAD
-  title: string;
-  description: string;
-  ratings: number[]; // Assuming ratings is an array of numbers
-  imageUrl: string;
-  timeInMins: number;
-  categories: string[];
-  instructions: string[];
-
-  ingredients: {
-    name: string;
-    amount: number;
-    unit: string;
-  }[];
-=======
     _id: string;
     title: string;
     description: string;
@@ -28,5 +13,4 @@ export interface RecipeInterface {
       amount: number;
       unit: string;
     }[];
->>>>>>> 1726100fbadd742e2af492eec761eeaab1585839
 }
