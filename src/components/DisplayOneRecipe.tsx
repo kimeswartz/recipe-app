f1ba3afbcaed2a660d399d07c84e5d4b7e659ab7
CasperWLX <hampuskallberg@gmail.com>
2024-03-14 11:39:45 +0100

    mega merge

--- src/components/DisplayOneRecipe.tsx
+++ src/components/DisplayOneRecipe.tsx
@@ -1,56 +1,112 @@
-import { RecipeInterface } from "../interfaces/RecipeInterface";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { RecipeInterface } from "../interfaces/RecipeInterface";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faStar } from "@fortawesome/free-solid-svg-icons";
+
+import "../styling/styleRecipePage.css";
 
 const DisplayOneRecipe: React.FC = () => {
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState<RecipeInterface | null>(null);
 
-    const { recipeId } = useParams();
-    const [recipe, setRecipe] = useState<RecipeInterface | null>(null);
-
-    useEffect(() => {
-        const fetchRecipe = async () => {
-        try {
-            const response = await fetch(
-            `https://sti-java-grupp4-s4yjx9.reky.se/recipes/${recipeId}`
-            );
-            const data: RecipeInterface = await response.json();
-            setRecipe(data);
-        } catch (error) {
-            console.error("Error fetching recipe:", error);
-        }
-        };
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await fetch(
+          `https://sti-java-grupp4-s4yjx9.reky.se/recipes/${recipeId}`
+        );
+        const data: RecipeInterface = await response.json();
+        setRecipe(data);
+      } catch (error) {
+        console.error("Error fetching recipe:", error);
+      }
+    };
 
     fetchRecipe();
   }, [recipeId]);
 
-    if (!recipe) return <div>Loading...</div>;
+  if (!recipe) return <div>Hämtar recept...</div>;
 
-    return (
-        <div>
-            <h1>{recipe.title}</h1>
-            <img src={recipe.imageUrl} alt={recipe.title} />
-            <p>{recipe.description}</p>
-            <p>Time to make: {recipe.timeInMins} minutes</p>
-            <h2>Ingredients</h2>
-            <ul>
-                {recipe.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient.amount} {ingredient.unit} {ingredient.name}</li>
-                ))}
-            </ul>
-            <h2>Instructions</h2>
-            <ol>
-                {recipe.instructions.map((instruction, index) => (
-                    <li key={index}>{instruction}</li>
-                ))}
-            </ol>
-            <h2>Categories</h2>
-            <ul>
-                {recipe.categories.map((category, index) => (
-                    <li key={index}>{category}</li>
-                ))}
-            </ul>
+  return (
+    <div className="recipe-container">
+      <div className="header-container">
+        <div className="text-container">
+          <h1>{recipe.title}</h1>
+          <p>{recipe.description}</p>
+
+          <div className="time-review-section">
+            <div className="info-container">
+              <div className="info-tag">
+                <p>
+                  <FontAwesomeIcon icon={faClock} className="clock-icon" />{" "}
+                  {recipe.timeInMins} Minuter
+                </p>
+              </div>
+            </div>
+
+            <div className="info-container">
+              <div className="info-tag">
+                <p>
+                  <FontAwesomeIcon icon={faStar} className="star-icon" /> 3/5
+                  Betyg
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="img-container">
+          <img src={recipe.imageUrl} alt={recipe.title} />
+        </div>
+      </div>
+
+      <div className="ingredients-container">
+        <div className="upper">
+          <h2>Du behöver...</h2>
+        </div>
+
+        <div className="lower">
+          <div className="centered-tags">
+            <ul className="list-objects">
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index} className="ingredient-name">
+                  {ingredient.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="instructions-section">
+        <div className="ingredients-wrapper">
+          <div className="centered-mobile">
+            <h2>Ingridienser</h2>
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  {ingredient.amount} {ingredient.unit} {ingredient.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="instructions-wrapper">
+          <div className="centered-mobile">
+            <h2>Gör såhär</h2>
+            <ol>
+              {recipe.instructions.map((instruction, index) => (
+                <li key= {index} className="to-do-step"> {instruction} </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DisplayOneRecipe;
