0493a2c340f0400efbff7bf78e033d431d16b61e
kimeswartz <kim.e.swartz@gmail.com>
2024-03-18 15:34:43 +0100

    updated json and app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
 import OldHomepage from "./pages/OldHomepage";
+import OneRecipePageContent from "./pages/oneRecipePage"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/testSite" element={<OldHomepage />}/>
+          <Route path="/displayRecipe" element={<OneRecipePageContent />}/>
         </Routes>
       </Router>
     </>
