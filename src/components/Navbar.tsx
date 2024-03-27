2aa554736e8aaeea1b625e7f092ec862aa54a63b
PabloDiaz <pablodiaz147@gmail.com>
2024-03-27 04:53:35 +0100

    CocktailAPI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,17 +9,6 @@ const Navbar = () => {
   };
 
   return (
-<<<<<<< HEAD
-   <div className="navbar container" >
-      <a className='logo'><Link to='/'>Logo</Link></a>
-      <div className='nav-links'>
-      <a><Link to='/'>Home</Link></a>
-      <a><Link to='/CategoryPage'>Categories</Link></a>
-      <a><Link to='/Filter'>Filter</Link></a>
-      <a><Link to='/Recipes'>Recipes</Link></a>
-      <a><Link to='/cocktails'>Cocktail</Link></a>
-      <a><Link to='/AdminPage'>Admin</Link></a>
-=======
     <div className="navbar container">
       <a className="logo" onClick={() => handleNavigation("/")}>
         Logo
@@ -27,10 +16,10 @@ const Navbar = () => {
       <div className="nav-links">
         <a onClick={() => handleNavigation("/")}>Home</a>
         <a onClick={() => handleNavigation("/CategoryPage")}>Categories</a>
+        <a onClick={() => handleNavigation("/Cocktails")}>Cocktails</a>
         <a onClick={() => handleNavigation("/Filter")}>Filter</a>
         <a onClick={() => handleNavigation("/Recipes")}>Recipes</a>
         <a onClick={() => handleNavigation("/AdminPage")}>Admin</a>
->>>>>>> 24390d7ba0d4fde873a53e5154c49a59cf4677ec
       </div>
     </div>
   );
