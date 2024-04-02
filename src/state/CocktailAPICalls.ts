db13ebdc9567300ba8b64b0b650ee0c58f077e63
CasperWLX <hampuskallberg@gmail.com>
2024-04-02 12:08:07 +0200

    cocktail globalstate

--- src/state/CocktailAPICalls.ts
+++ src/state/CocktailAPICalls.ts
@@ -4,19 +4,22 @@ import { create } from 'zustand';
 import CocktailInterface from '../interfaces/CocktailInterfaces/CocktailInterface';
 import axios from 'axios';
 import { cocktailURL } from '../constants/ApiUrl';
+import IngredientInterface from '../interfaces/CocktailInterfaces/IngredientInterface';
 
 interface CocktailStateInterface {
   cocktailList: CocktailInterface[];
   oneCocktail: CocktailInterface;
-  //oneIngredient: IngredientInterface;
+  oneIngredient: IngredientInterface;
   fetchAllCocktails: () => Promise<void>;
   fetchCocktailByName: (cocktailName: string) => Promise<void>;
   fetchIngredient: (ingredientName: string) => Promise<void>;
+  setOneCocktail: (cocktail: CocktailInterface) => void;
 }
 
 const globalCocktailFunctions = create<CocktailStateInterface>()((set) => ({
   cocktailList: [],
   oneCocktail: {} as CocktailInterface,
+  oneIngredient: {} as IngredientInterface,
 
   fetchAllCocktails: async() => {
     try{
@@ -42,8 +45,20 @@ const globalCocktailFunctions = create<CocktailStateInterface>()((set) => ({
   }, 
 
   fetchIngredient: async(name) => {
-    
-  }
+    try{
+      const response = await axios.get(`${cocktailURL}/search.php?i=${name}`)
+      if(response.status === 200){
+        console.log(response.data.ingredients[0])
+        set({ oneIngredient: response.data.ingredients[0]})
+      }
+    }catch(error){
+      console.log('something went wrong:', error)
+    }
+  },
+
+  setOneCocktail: (newCocktail) => {
+    set({ oneCocktail: newCocktail})
+  },
 }))
 
 export default globalCocktailFunctions;
