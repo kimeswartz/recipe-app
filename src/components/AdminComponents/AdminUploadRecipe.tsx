54233ee4c5ee5a63281d994ea0d4c7c5ffba5f32
CasperWLX <hampuskallberg@gmail.com>
2024-04-02 15:40:15 +0200

    pre pull

--- src/components/AdminComponents/AdminUploadRecipe.tsx
+++ src/components/AdminComponents/AdminUploadRecipe.tsx
@@ -1,160 +1,95 @@
 import { useState } from "react";
 import allRecipeState from "../../store/Endpoints";
-import { RecipeInterface } from "../../interfaces/RecipeInterface";
+import uploadUpdateRecipeState from "../../store/UpdateAndUpload";
 
 // Destructuring addRecipe function from the Zusand store, with direct access to the function in /Endpoints
 const UploadRecipe = () => {
   const { addRecipe } = allRecipeState();
 
   // state to manage form data for uploading a recipe
-  const [recipeData, setRecipeData] = useState<RecipeInterface>({
-    title: "",
-    description: "",
-    ratings: [],
-    imageUrl: "",
-    timeInMins: 0,
-    categories: [],
-    instructions: [],
-    ingredients: [
-      {
-        name: "",
-        amount: 0,
-        unit: "",
-      },
-    ],
-  });
 
-  // Event handler function to handle input updates for recipe fields
-  const handleInputUpdate = (input: any) => {
-    const { name, value } = input.target;
+  const { recipe, setTitle, setDescription, setImageUrl, setTimeInMins, setCategories, setInstructions, setIngredients, emptyRecipe } = uploadUpdateRecipeState();
 
-    setRecipeData((updateData) => ({
-      ...updateData,
-      [name]: value,
-    }));
-  };
+  const [categoryList, setCategoryList] = useState<string[]>([]);
 
-  // Function to add a new ingredient input to the recipe
-  const handleIngredientInput = (index: number, field: string, value: any) => {
-    setRecipeData((prevData) => {
-      const updatedIngredients = [...prevData.ingredients];
-      updatedIngredients[index] = {
-        ...updatedIngredients[index],
-        [field]: value,
-      };
-
-      return {
-        ...prevData,
-        ingredients: updatedIngredients,
-      };
-    });
-  };
-
-  // Function to add a new ingredient to the recipe
-  const addIngredient = () => {
-    setRecipeData((prevData) => ({
-      ...prevData,
-      ingredients: [...prevData.ingredients, { name: "", amount: 0, unit: "" }],
-    }));
-  };
-
-  // Function to handle input updates for instruction fields
-  const handleInstructionInput = (index: number, value: string) => {
-    setRecipeData((prevData) => {
-      const updateInstructions = [...prevData.instructions];
-      updateInstructions[index] = value;
-
-      return {
-        ...prevData,
-        instructions: updateInstructions,
-      };
-    });
-  };
-
-  // Function to add a new instruction to the recipe
-  const addInstruction = () => {
-    setRecipeData((prevData) => ({
-      ...prevData,
-      instructions: [...prevData.instructions, ""],
-    }));
-  };
 
   // Function to handle form submission
   const handleSubmit = async (clickEvent: any) => {
     clickEvent.preventDefault();
 
-    try {
-      // Checking if any required fields are empty
-      if (
-        recipeData.title === "" ||
-        recipeData.description === "" ||
-        recipeData.imageUrl === "" ||
-        recipeData.timeInMins === 0 ||
-        recipeData.instructions.some((instruction: string) => instruction === "") ||
-        recipeData.ingredients.some(
-          (ingredient) => ingredient.name === "" || ingredient.amount === 0 || ingredient.unit === ""
-        ) ||
-        recipeData.categories.length === 0
-      ) {
-        throw new Error("Ett fält är tomt");
-      }
-
-      // Adding the recipe data to the database
-      addRecipe(recipeData);
-
-      // Resetting the form fields after submission
-      setRecipeData({
-        title: "",
-        description: "",
-        ratings: [],
-        imageUrl: "",
-        timeInMins: 0,
-        categories: [],
-        instructions: [],
-        ingredients: [
-          {
-            name: "",
-            amount: 0,
-            unit: "",
-          },
-        ],
-      });
-    } catch (error) {
-      alert("Vänligen fyll i alla fält");
-      return;
+    if (
+      recipe.title === "" ||
+      recipe.description === "" ||
+      recipe.imageUrl === "" ||
+      recipe.timeInMins === 0 ||
+      recipe.instructions.some((instruction: string) => instruction === "") ||
+      recipe.ingredients.some(
+        (ingredient) => ingredient.name === "" || ingredient.amount === 0 || ingredient.unit === ""
+      ) ||
+      recipe.categories.length === 0
+    ) {
+      throw new Error("One field is empty");
+    } else {
+      addRecipe(recipe);
     }
+
+    // Adding the recipe data to the database
+
+    // Resetting the form fields after submission
   };
 
   // Preset categories for recipes
   const presetCategories = [
-    "Frukost",
+    "Breakfast",
     "Lunch",
-    "Middag",
+    "Dinner",
     "Vegetarian",
     "Party",
-    "Asiatisk",
+    "Asian",
     "Latin American",
   ];
 
   // Present unit for ingredients
-  const presentIngredientsUnit = ["l", "dl", "ml", "msk", "tsk", "g", "kg", "st"];
+  const presentIngredientsUnit = ["l", "dl", "ml", "tbsp", "tsp", "g", "kg", "noOf"];
 
   // Function to handle category changes
   const handleCategoryChange = (selectedCategory: string) => {
-    setRecipeData((prevData) => {
-      let updatedCategories;
+    const updatedCategories = recipe.categories.includes(selectedCategory)
+      ? recipe.categories.filter(category => category !== selectedCategory)
+      : [...recipe.categories, selectedCategory]
 
-      if (prevData.categories.includes(selectedCategory)) {
-        updatedCategories = prevData.categories.filter((category) => category !== selectedCategory);
-      } else {
-        updatedCategories = [...prevData.categories, selectedCategory];
-      }
+    setCategories(updatedCategories);
+  };
 
-      return {
-        ...prevData,
-        categories: updatedCategories,
-      };
-    });
+  const handleInstructionsChange = (newInstruction: string) => {
+    const updatedInstructions = recipe.instructions.includes(newInstruction)
+      ? recipe.instructions.filter(instruction => instruction !== newInstruction)
+      : [...recipe.instructions, newInstruction];
+
+    updatedInstructions.filter(instruction => instruction === '')
+
+    setInstructions(updatedInstructions);
+  }
+
+  const handleIngredientChange = (index: number, field: string, value: string | number) => {
+    const updatedIngredients = [...recipe.ingredients];
+    updatedIngredients[index] = {
+      ...updatedIngredients[index],
+      [field]: value,
+    }
+    setIngredients(updatedIngredients)
+  }
+
+  const handleAddIngredient = () => {
+    const newIngredient = { name: '', amount: 0, unit: '' };
+    const updatedIngredients = [...recipe.ingredients, newIngredient];
+    setIngredients(updatedIngredients);
+  };
+
+  const handleAddInstruction = () => {
+    const newInstruction = '';
+    const updatedInstructions = [...recipe.instructions, newInstruction];
+    setInstructions(updatedInstructions)
   };
 
   return (
@@ -168,8 +103,7 @@ const UploadRecipe = () => {
               className="user-input"
               type="text"
               name="title"
-              value={recipeData.title}
-              onChange={handleInputUpdate}
+              onChange={(input) => setTitle(input.target.value)}
             />
           </label>
           <label className="upload-label">
@@ -178,8 +112,7 @@ const UploadRecipe = () => {
               className="user-input"
               type="text"
               name="description"
-              value={recipeData.description}
-              onChange={handleInputUpdate}
+              onChange={(input) => setDescription(input.target.value)}
             />
           </label>
           <label className="upload-label">
@@ -188,8 +121,7 @@ const UploadRecipe = () => {
               className="user-input"
               type="text"
               name="imageUrl"
-              value={recipeData.imageUrl}
-              onChange={handleInputUpdate}
+              onChange={(input) => setImageUrl(input.target.value)}
             />
           </label>
           <label className="upload-label">
@@ -198,8 +130,7 @@ const UploadRecipe = () => {
               className="user-input"
               type="number"
               name="timeInMins"
-              value={recipeData.timeInMins}
-              onChange={handleInputUpdate}
+              onChange={(input) => setTimeInMins(parseInt(input.target.value))}
             />
           </label>
 
@@ -210,7 +141,7 @@ const UploadRecipe = () => {
                 className="category-checkbox"
                 type="checkbox"
                 value={category}
-                checked={recipeData.categories.includes(category)}
+                checked={recipe.categories.includes(category)}
                 onChange={(e) => handleCategoryChange(e.target.value)}
               />
               {category}
@@ -218,7 +149,7 @@ const UploadRecipe = () => {
           ))}
 
           <h2 className="upload-h2">Instructions</h2>
-          {recipeData.instructions.map((instruction, index) => (
+          {recipe.instructions.map((instruction, index) => (
             <div key={index}>
               <label className="upload-label">
                 Instruction {index + 1}:
@@ -226,9 +157,7 @@ const UploadRecipe = () => {
                   className="user-input"
                   type="text"
                   value={instruction}
-                  onChange={(e) =>
-                    handleInstructionInput(index, e.target.value)
-                  }
+                  onChange={(e) => handleInstructionsChange(e.target.value)}
                 />
               </label>
             </div>
@@ -236,7 +165,7 @@ const UploadRecipe = () => {
           <button
             className="upload-button"
             type="button"
-            onClick={addInstruction}
+            onClick={handleAddInstruction}
           >
             Add Instruction
           </button>
@@ -244,7 +173,7 @@ const UploadRecipe = () => {
           <br />
 
           <h2 className="upload-h2">Ingredients</h2>
-          {recipeData.ingredients.map((ingredient, index) => (
+          {recipe.ingredients.map((ingredient, index) => (
             <div key={index}>
               <label className="upload-label">
                 Ingredient Name:
@@ -252,33 +181,18 @@ const UploadRecipe = () => {
                   className="user-input"
                   type="text"
                   value={ingredient.name}
-                  onChange={(enteredByUser) =>
-                    handleIngredientInput(
-                      index,
-                      "name",
-                      enteredByUser.target.value
-                    )
-                  }
+                  onChange={(input) => handleIngredientChange(index, 'name', input.target.value)}
                 />
               </label>
 
-
               <div className="amount-unit-container">
-
-
                 <label className="upload-label">
                   Amount:
                   <input
                     className="user-input"
                     type="number"
                     value={ingredient.amount}
-                    onChange={(enteredByUser) =>
-                      handleIngredientInput(
-                        index,
-                        "amount",
-                        +enteredByUser.target.value
-                      )
-                    }
+                    onChange={(enteredByUser) => handleIngredientChange(index, "amount", enteredByUser.target.value)}
                   />
                 </label>
 
@@ -287,13 +201,7 @@ const UploadRecipe = () => {
                   <select
                     className="user-input"
                     value={ingredient.unit}
-                    onChange={(selectedByUser) =>
-                      handleIngredientInput(
-                        index,
-                        "unit",
-                        selectedByUser.target.value
-                      )
-                    }
+                    onChange={(selectedByUser) => handleIngredientChange(index, "unit", selectedByUser.target.value)}
                   >
                     {presentIngredientsUnit.map((unit, index) => (
                       <option key={index} value={unit}>
@@ -311,7 +219,7 @@ const UploadRecipe = () => {
           <button
             className="upload-button"
             type="button"
-            onClick={addIngredient}
+            onClick={handleAddIngredient}
           >
             Add Ingredient
           </button>
