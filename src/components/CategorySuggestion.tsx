7f71060541196652a8e09b36ab6c5f7e4eab97af
kimeswartz <kim.e.swartz@gmail.com>
2024-04-03 09:58:49 +0200

    delete buttons fixed

--- src/components/CategorySuggestion.tsx
+++ src/components/CategorySuggestion.tsx
@@ -5,7 +5,8 @@ import allRecipeState from "../state/Endpoints";
 
 const CategorySuggestion = () => {
   const navigate = useNavigate();
-  const { categoryList, recipeList, fetchAllCategories, fetchOneCategory } = allRecipeState();
+  const { categoryList, recipeList, fetchAllCategories, fetchOneCategory } =
+    allRecipeState();
 
   // To fetch all categories when component first loads or when the categoryList changes
   useEffect(() => {
@@ -20,19 +21,19 @@ const CategorySuggestion = () => {
   };
 
   return (
-<div className="tag-grid category-suggestion-container">
-  {categoryList.map((category, index) => (
-    <div
-      key={index}
-      onClick={() => handleCategoryClick(category.name)}
-      className="recipe-tag category"
-    >
-      <div className="tag-div">
-      <h3>{category.name}</h3>
-      </div>
+    <div className="tag-grid category-suggestion-container">
+      {categoryList.map((category, index) => (
+        <div
+          key={index}
+          onClick={() => handleCategoryClick(category.name)}
+          className="recipe-tag category"
+        >
+          <div className="tag-div">
+            <h3>{category.name}</h3>
+          </div>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
   );
 };
 
