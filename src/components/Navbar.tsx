096df18a62d0a8af2bbc7da641e8942de44d1496
CasperWLX <hampuskallberg@gmail.com>
2024-04-08 17:01:32 +0200

    more bug fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import "../styling/NavbarStyle.css";
 import { To, useNavigate } from "react-router-dom";
 import globalCartFunctions from "../store/GlobalCart"
+import uploadUpdateRecipeState from "../store/GlobalUpdateAndUpload";
 import ShoppingList from '../assets/Shoppinglist.svg'
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { emptyRecipe } = uploadUpdateRecipeState();
 
   const handleNavigation = (path: To) => {
     navigate(path);
+    emptyRecipe();
   };
 
   const { displayCart, toggleCart } = globalCartFunctions();
@@ -24,7 +27,7 @@ const Navbar = () => {
         <a onClick={() => handleNavigation("/recipes")}>Recipes</a>
         <a onClick={() => handleNavigation("/adminpage")}>Admin</a>
         <a onClick={() => handleNavigation("/popular")}>Popular</a>
-        <a onClick={() => toggleCart(displayCart)} className="list-container"><img src={ShoppingList} alt="Shopping list"/></a>
+        <a onClick={() => toggleCart(displayCart)} className="list-container"><img src={ShoppingList} alt="Shopping list" /></a>
       </div>
     </div>
   );
