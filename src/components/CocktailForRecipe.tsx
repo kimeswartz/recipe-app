d69c54b0819314bf9c9489ea9f82c9dee6a99c61
PabloDiaz <pablodiaz147@gmail.com>
2024-04-09 22:04:28 +0200

    CocktailForRecipe

--- src/components/CocktailForRecipe.tsx
+++ src/components/CocktailForRecipe.tsx
@@ -1,35 +1,46 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 import globalRecipeFunctions from "../store/RecipeAPICalls";
 import globalCocktailFunctions from "../store/CocktailAPICalls";
+import { useNavigate } from "react-router-dom";
 
 
 const CocktailForRecipe = () => {
 
   const { oneRecipe } = globalRecipeFunctions();
-  const { cocktailsByIngredient, fetchCocktailsByIngredient } = globalCocktailFunctions();
-
+  const { oneCocktail, setOneCocktail, cocktailsByIngredient, fetchCocktailsByIngredient } = globalCocktailFunctions();
+  
+  const navigate = useNavigate();
 
   const ingredientVariable = 'Vodka';
 
   useEffect(() => {
-    fetchCocktailsByIngredient(ingredientVariable);
+    fetchCocktailsByIngredient(ingredientVariable).then(() => {
+      let recipeValue = oneRecipe.price;
+      console.log(recipeValue)
+      while(recipeValue > 94){
+        recipeValue = recipeValue / 3;
+      }
+      recipeValue = Math.trunc(recipeValue);
+
+      if(recipeValue === undefined || recipeValue === 0 || isNaN(recipeValue)){
+        recipeValue = 69;
+      }
+
+      setOneCocktail(cocktailsByIngredient[recipeValue])
+      console.log('this is the value', recipeValue)
+    })
     console.log('all vodka cocktails', cocktailsByIngredient)
-    pickARandomDrink();
-  }, []);
+  }, [oneRecipe]);
 
-  const pickARandomDrink = () => {
-    if(oneRecipe)
+  const handleNavigate = () => {
+    navigate(`/cocktail/${oneCocktail?.idDrink}`)
+    window.scrollTo(0, 0);
   }
-
 
   return (
     <div>
-      {/* <h2>{cocktail.strDrink}</h2> */}
-      {/* <p>{cocktail.strInstructions}</p> */}
       <div>
-        <h3>Recommended Cocktail for :</h3>
-        {/* <p>{categoryCocktail}</p> */}
+        <h3 onClick={handleNavigate}>To this recipe we recommend : {oneCocktail?.strDrink}</h3>
       </div>
     </div>
   );
