5d186c9b364677b2b1ea7d5c258a88c1f8b53764
aliceh0155 <hofverbergalice@gmail.com>
2024-04-09 22:06:13 +0200

    new test and id's

---
+++ cypress/e2e/testRoute.cy.ts
@@ -0,0 +1,22 @@
+describe("test route about", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get('#footerNavigateAbout').click();
+
+    cy.location("pathname").should("equal", "/about");
+    
+    
+  });
+}); 
+
+describe("test route filter", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get('#headerNavigateFilter').click();
+
+    cy.location("pathname").should("equal", "/filter");
+    
+  });
+}); 
