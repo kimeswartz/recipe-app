42df07460840723c7ae091d0fa56c5bc3b790e74
kimeswartz <kim.e.swartz@gmail.com>
2024-04-10 15:38:49 +0200

    updated css recommended cocktail

--- src/components/cocktail_components/CocktailForRecipe.tsx
+++ src/components/cocktail_components/CocktailForRecipe.tsx
@@ -3,15 +3,18 @@ import globalRecipeFunctions from "../../store/RecipeAPICalls";
 import globalCocktailFunctions from "../../store/CocktailAPICalls";
 import { useNavigate } from "react-router-dom";
 
-
 const CocktailForRecipe = () => {
-
   const { oneRecipe } = globalRecipeFunctions();
-  const { oneCocktail, setOneCocktail, cocktailsByIngredient, fetchCocktailsByIngredient } = globalCocktailFunctions();
+  const {
+    oneCocktail,
+    setOneCocktail,
+    cocktailsByIngredient,
+    fetchCocktailsByIngredient,
+  } = globalCocktailFunctions();
 
   const navigate = useNavigate();
 
-  const ingredientVariable = 'Vodka';
+  const ingredientVariable = "Vodka";
 
   useEffect(() => {
     fetchCocktailsByIngredient(ingredientVariable).then(() => {
@@ -20,23 +23,30 @@ const CocktailForRecipe = () => {
         recipeValue = recipeValue / 3;
       }
       recipeValue = Math.trunc(recipeValue);
-      
-      if (recipeValue === undefined || recipeValue === 0 || isNaN(recipeValue)) {
+
+      if (
+        recipeValue === undefined ||
+        recipeValue === 0 ||
+        isNaN(recipeValue)
+      ) {
         recipeValue = 69;
       }
-      setOneCocktail(cocktailsByIngredient[recipeValue])
-    })
+      setOneCocktail(cocktailsByIngredient[recipeValue]);
+    });
   }, [oneRecipe]);
 
   const handleNavigate = () => {
-    navigate(`/cocktail/${oneCocktail?.idDrink}`)
+    navigate(`/cocktail/${oneCocktail?.idDrink}`);
     window.scrollTo(0, 0);
-  }
+  };
 
   return (
     <div>
       <div>
-        <h3 onClick={handleNavigate}>To this recipe we recommend : {oneCocktail?.strDrink}</h3>
+        <p onClick={handleNavigate}>To this recipe we recommend:</p>
+        <p className="pointer">
+          <strong>{oneCocktail?.strDrink}</strong>
+        </p>
       </div>
     </div>
   );
