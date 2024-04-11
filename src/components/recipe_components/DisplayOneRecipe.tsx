504b1e9aaede0b9517f9a2379f42c4143ce676f7
PabloDiaz <pablodiaz147@gmail.com>
2024-04-11 16:22:03 +0200

    URLFAILED

--- src/components/recipe_components/DisplayOneRecipe.tsx
+++ src/components/recipe_components/DisplayOneRecipe.tsx
@@ -12,8 +12,12 @@ import CommentComponent from "../recipe_components/CommentComponent";
 // Component for displaying a single recipe
 const DisplayOneRecipe = () => {
   // Destructuring state and function from the state management
-  const { oneRecipe, fetchOneRecipe, addRating, fetchComments } =
-    globalRecipeFunctions();
+  const {
+    oneRecipe,
+    fetchOneRecipe,
+    addRating,
+    fetchComments,
+  } = globalRecipeFunctions();
   const { addRecipeToCart } = globalCartFunctions();
 
   // Extracting recipeId from URL params
@@ -39,152 +43,143 @@ const DisplayOneRecipe = () => {
     setUserRating(rating);
   };
 
+
+
   // Conditional rendering based on whether the recipe has loaded or not
   if (!oneRecipe) {
     return <div>Loading recipe...</div>; // Display loading message
   } else {
     return (
-      <>
-        <section className="standard-container">
-          <h1>{oneRecipe.title}</h1> {/* Display recipe title */}
-        </section>
-
-        <section className="standard-container">
-          <div className="flex-header-container">
-            <div className="text-container">
-              <p>{oneRecipe.description}</p> {/* Display recipe description */}
-              {/* Display recipe time and rating */}
-              <div className="time-review-section">
-                <div className="info-container">
-                  <div className="info-tag">
-                    <p>
-                      <FontAwesomeIcon icon={faClock} className="clock-icon" />{" "}
-                      {oneRecipe.timeInMins} Minutes
-                    </p>
-                  </div>
-                </div>
-
-                <div className="info-container">
-                  <div className="info-tag">
-                    <p>
-                      <FontAwesomeIcon icon={faStar} className="star-icon" />{" "}
-                      {oneRecipe.avgRating !== null ? (
-                        <span>{oneRecipe.avgRating?.toFixed(1)}/5</span>
-                      ) : (
-                        <span>Missing grade</span>
-                      )}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="info-container">
-                  <div className="info-tag">
-                    {/* Interaktiv ratingsystem */}
-                    <p>
-                      Rate this dish: {""}
-                      {[1, 2, 3, 4, 5].map((value) => (
-                        <span
-                          key={value}
-                          style={{ cursor: "pointer" }}
-                          onClick={() => handleRatingChange(value)}
-                        >
-                          <FontAwesomeIcon
-                            icon={faStar}
-                            className="star-icon"
-                            color={
-                              value <= (userRating || 0) ? "yellow" : "red"
-                            }
-                          />
-                        </span>
-                      ))}
-                    </p>
-                  </div>
-                </div>
-                <div className="spacer-container">
-                  <CocktailForRecipe />
+      <div className="recipe-container">
+        {/* Header section displaying recipe title, description, time, and rating */}
+        <div className="header-container">
+          <div className="text-container">
+            <h1>{oneRecipe.title}</h1> {/* Display recipe title */}
+            <p>{oneRecipe.description}</p> {/* Display recipe description */}
+            {/* Display recipe time and rating */}
+            <div className="time-review-section">
+              <div className="info-container">
+                <div className="info-tag">
+                  <p>
+                    <FontAwesomeIcon icon={faClock} className="clock-icon" />{" "}
+                    {oneRecipe.timeInMins} Minutes
+                  </p>
                 </div>
               </div>
-              <div className="spacer-container">
-                <button
-                  onClick={() => addRecipeToCart(oneRecipe)}
-                  className="main-button"
-                >
-                  Add to list
-                </button>
-              </div>
-            </div>
 
-            {/* Display recipe image */}
-            <div className="img-container">
-              <img src={oneRecipe.imageUrl} alt={oneRecipe.title} />
+              <div className="info-container">
+                <div className="info-tag">
+                  <p>
+                    <FontAwesomeIcon icon={faStar} className="star-icon" />{" "}
+                    {oneRecipe.avgRating !== null ? (
+                      <span>{oneRecipe.avgRating?.toFixed(1)}/5</span>
+                    ) : (
+                      <span>Missing grade</span>
+                    )}
+                  </p>
+                </div>
+              </div>
+
+              <div className="info-container">
+                <div className="info-tag">
+                  {/* Interaktiv ratingsystem */}
+                  <p>
+                    Rate this dish: {""}
+                    {[1, 2, 3, 4, 5].map((value) => (
+                      <span
+                        key={value}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleRatingChange(value)}
+                      >
+                        <FontAwesomeIcon
+                          icon={faStar}
+                          className="star-icon"
+                          color={value <= (userRating || 0) ? "yellow" : "red"}
+                        />
+                      </span>
+                    ))}
+                  </p>
+                </div>
+                <div className="info-container">
+                  <button
+                    onClick={() => addRecipeToCart(oneRecipe)}
+                    className="main-button"
+                  >
+                    Add to list
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </section>
 
+          {/* Display recipe image */}
+          <div className="img-container">
+            <img src={oneRecipe.imageUrl} alt={oneRecipe.title} />
+          </div>
+        </div>
         {/* Section displaying ingredients */}
-        <section className="standard-container green-background">
+        <div className="ingredients-container">
           <div className="upper">
             <h2>You need...</h2>
           </div>
 
           <div className="lower">
             {/* List ingredients */}
-            <div className="spacer-container">
-              <div className="centered-tags">
-                <ul className="list-objects">
-                  {oneRecipe.ingredients?.map((ingredient, index) => (
-                    <li key={index} className="ingredient-name">
-                      {ingredient.name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+            <div className="centered-tags">
+              <ul className="list-objects">
+                {oneRecipe.ingredients?.map((ingredient, index) => (
+                  <li key={index} className="ingredient-name">
+                    {ingredient.name}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-        </section>
-
-        {/* Instructions Section*/}
-        <section className="standard-container">
-          <div className="flex-container">
-            <div className="ingredients-wrapper">
-              <div className="centered-mobile">
-                <h2>Ingredients</h2>
-                <ul>
-                  {oneRecipe.ingredients?.map((ingredient, index) => (
-                    <li key={index}>
-                      {ingredient.amount} {ingredient.unit} {ingredient.name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-
-            {/* Section for displaying instructions */}
-            <div className="instructions-wrapper">
-              <div className="centered-mobile">
-                <h2>How to do it...</h2>
-                <ol>
-                  {/* Step-by-step Section */}
-                  {oneRecipe.instructions?.map((instruction, index) => (
-                    <li key={index} className="to-do-step">
-                      {" "}
-                      {instruction}{" "}
-                    </li>
-                  ))}
-                </ol>
-              </div>
+        </div>
+        {/* Section displaying instructions */}
+        <div className="instructions-section">
+          {/* Section for displaying ingredients */}
+          <div className="ingredients-wrapper">
+            <div className="centered-mobile">
+              <h2>Ingredients</h2>
+              <ul>
+                {/* Display ingredients with amount and unit */}
+                {oneRecipe.ingredients?.map((ingredient, index) => (
+                  <li key={index}>
+                    {ingredient.amount} {ingredient.unit} {ingredient.name}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-        </section>
 
-        {/* Comments Section */}
-        <section className="standard-container">
+          {/* Section for displaying instructions */}
+          <div className="instructions-wrapper">
+            <div className="centered-mobile">
+              <h2>Do like this</h2>
+              <ol>
+                {/* Display step-by-step instructions */}
+                {oneRecipe.instructions?.map((instruction, index) => (
+                  <li key={index} className="to-do-step">
+                    {" "}
+                    {instruction}{" "}
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </div>
+        </div> 
+        {/* Section for displaying comments */}
+        <div className="comments-section"> 
           <div className="adjust-content-with">
-            <CommentComponent />
+            <CommentComponent
+            />
+            </div>
           </div>
-        </section>
-      </>
+          <CocktailForRecipe /> {/* Använd CocktailForRecipe */}
+        </div>
     );
+    
   }
 };
 
