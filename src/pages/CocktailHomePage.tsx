84a028b9bb3f939687d599c3d00c3b1c2f6fb3eb
CasperWLX <hampuskallberg@gmail.com>
2024-04-11 22:53:48 +0200

    cocktail page

--- src/pages/CocktailHomePage.tsx
+++ src/pages/CocktailHomePage.tsx
@@ -4,13 +4,8 @@ import ListCocktailsAlphabet from "../components/cocktail_components/ListCocktai
 const CocktailHomePage = () => {
   return (
     <div>
-      <div className="spacer-container">
-        <h1>Our Cocktails</h1>
-      </div>
-
       <ListCocktailsAlphabet />
       <CocktailLetters />
-      
     </div>
   );
 };
