c4a8e883b51d271159ca6d4f32c89946581cdbe2
CasperWLX <hampuskallberg@gmail.com>
2024-04-16 09:59:19 +0200

    navigation fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,6 +15,7 @@ const Navbar = () => {
 
   const handleNavigation = (path: string) => {
     navigate(path);
+    window.scrollTo(0,0)
     emptyRecipe();
     setIsResponsive(false); // Close the responsive menu on navigation
   };
